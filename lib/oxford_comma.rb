a10399807c094b035d16bf0760bfcbba99903c02
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-19 01:54:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,8 @@ if array.length == 1
   array.join('')
 elsif array.length == 2
   array.join(" and ")
-elsif array.length => 3
+else
   array[-1] = "and #{array[-1]}"
       array.join(', ')
-else
-  nil
   end
 end
