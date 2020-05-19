7448cd0356af29de9706c54275a217cfbeea5068
Serhiy Bardysh <sbardysh@tutanota.com>
2020-05-19 01:54:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ if array.length == 1
   array.join('')
 elsif array.length == 2
   array.join(" and ")
-else
-  array.length => 3
+elsif array.length => 3
   array[-1] = "and #{array[-1]}"
       array.join(', ')
   
